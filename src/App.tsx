cf285429f68dad1e409eee1d31aadf3202ce1cfa
skythrill256 <anjishnuganguly773@gmail.com>
2025-05-19 09:06:35 +0530

    added github actions and hibiscus web-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,6 +9,7 @@ import PricingPage from "./pages/PricingPage";
 import ComingSoonPage from "./pages/ComingSoonPage";
 import ArticlesPage from "./pages/ArticlesPage";
 import ProductPage from "./pages/ProductPage";
+import AgentRegistry from "./components/AgentRegistry";
 
 const queryClient = new QueryClient();
 
@@ -22,7 +22,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/pricing" element={<PricingPage />} />
-          <Route path="/hibiscus" element={<ComingSoonPage />} />
+          <Route path="/hibiscus" element={<AgentRegistry />} />
           <Route path="/articles" element={<ArticlesPage />} />
           <Route path="/product" element={<ProductPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
